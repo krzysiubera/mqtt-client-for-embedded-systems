8de22eae4ecde960d4e1968c1175441da23f7083
Krzysztof Bera <krzysztof.bera9@gmail.com>
2023-08-28 17:35:31 +0200

    configure pin PE2 as digital output

--- MQTTClient/Inc/mqtt_config.h
+++ MQTTClient/Inc/mqtt_config.h
@@ -1,9 +1,9 @@
 #ifndef MQTT_CONFIG_H
 #define MQTT_CONFIG_H
 
-#define MQTT_REQUESTS_QUEUE_LEN 9
-#define MQTT_MAX_PAYLOAD_LEN 17
-#define MQTT_MAX_TOPIC_LEN 15
+#define MQTT_REQUESTS_QUEUE_LEN 2
+#define MQTT_MAX_PAYLOAD_LEN 53
+#define MQTT_MAX_TOPIC_LEN 12
 
 /* max_payload_len and max_topic_len - has to include '\0' char in it */
 
